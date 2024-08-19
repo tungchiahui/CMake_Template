77a058b841c74f411218fb5cf71c20c4b61aeacd
TungChiahui <tungchiahui@gmail.com>
2024-08-20 00:55:14 +0800

    Fix Run

--- build/src/Matrix/CMakeFiles/Matrix_src_lib.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for Matrix_src_lib.
-# This may be replaced when dependencies are built.
--- src/main.cpp
+++ src/main.cpp
@@ -9,5 +9,8 @@ int main(int argc,char **argv)
     funcc();
     eigen3_test();
     cout << "666!" << std::endl;
+    
+    std::cout << "Press Enter to continue...";
+    std::cin.get(); // 等待用户按下回车键
     return 0;
 }
