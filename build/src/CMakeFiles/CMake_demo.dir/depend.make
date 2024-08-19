175296ab8fa0c931bd78f3fd64c8b8dedf6a23f3
TungChiahui <tungchiahui@gmail.com>
2024-08-19 12:00:41 +0800

    fix install dir

--- build/src/CMakeFiles/CMake_demo.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CMake_demo.
-# This may be replaced when dependencies are built.
--- build/src/Func1/CMakeFiles/Func1_src_lib.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for Func1_src_lib.
