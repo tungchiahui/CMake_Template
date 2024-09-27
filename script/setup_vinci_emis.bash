72f0ef94a85d5337d1c6ecb7abc1c0e28e0a54a8
tungchiahui <tungchiahui@gmail.com>
2024-09-27 23:54:42 +0800

    Set LD_LIBRARY_PATH

--- script/setup_vinci_emis.bash
+++ script/setup_vinci_emis.bash
@@ -26,10 +26,7 @@ _vinci_emis() {
 # 获取当前脚本所在目录的绝对路径
 SCRIPT_ABS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# 使用相对路径设置 LD_LIBRARY_PATH
-export LD_LIBRARY_PATH="${SCRIPT_ABS_DIR}/../install/lib:$LD_LIBRARY_PATH"
-
-# 输出 LD_LIBRARY_PATH 以验证设置
-# echo "LD_LIBRARY_PATH is set to: $LD_LIBRARY_PATH"
+# 设置 LD_LIBRARY_PATH
+source ${SCRIPT_ABS_DIR}/../install/.setup.bash
 
 complete -F _vinci_emis ../script/vinci_emis
