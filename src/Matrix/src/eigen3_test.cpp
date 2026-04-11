cd2d7cdc223613ba8a6acdc5945bb6d6b564a1a4
tungchiahui <tungchiahui@gmail.com>
2026-04-11 10:06:12 +0800

    update

--- src/Matrix/src/eigen3_test.cpp
+++ src/Matrix/src/eigen3_test.cpp
@@ -4,13 +4,7 @@
 
 
 
-void eigen3_test(void)
+void main_test(void)
 {
-    // 创建 2x2 矩阵
-    Eigen::Matrix2d mat;
-    mat << 1, 2,
-           6, 4;
-
-    // 打印矩阵
-    std::cout << "打印一个矩阵:\n" << mat << std::endl;
+    
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,77 @@
 #include <iostream>
-#include "Func1/inc/testFunc.h"
-#include "Matrix/inc/eigen3_test.hpp"
+#include <functional>
+#include <memory>
 
-using std::cout;
 
-int main(int argc,char **argv) 
+// 基类
+class BaseCalculator
 {
-    funcc();
-    eigen3_test();
-    cout << "666!" << std::endl;
-    
-    std::cout << "Press Enter to continue...";
-    std::cin.get(); // 等待用户按下回车键
+public:
+    void set_callback(std::function<int(int,int)> cb)
+    {
+        callback_ = std::move(cb);
+    }
+
+    std::function<int(int,int)> get_callback() const
+    {
+        return callback_;
+    }
+
+    void do_calculation(int x, int y)
+    {
+        if (callback_)
+        {
+            int result = callback_(x, y);
+            std::cout << "Result: " << result << std::endl;
+        }
+    }
+
+private:
+    std::function<int(int,int)> callback_;
+};
+
+// 派生类：加法
+class AddCalculator : public BaseCalculator
+{
+public:
+    AddCalculator()
+    {
+        set_callback(std::bind(&AddCalculator::add,this,std::placeholders::_1, std::placeholders::_2));
+    }
+
+private:
+    int add(int a, int b)
+    {
+        return a + b;
+    }
+};
+
+// 派生类：乘法
+class MultiplyCalculator : public BaseCalculator
+{
+public:
+    MultiplyCalculator()
+    {
+        set_callback([this](int a, int b) 
+        {
+            return a * b;
+        });
+    }
+
+};
+
+// 主函数
+int main()
+{
+    auto add_calc = std::make_shared<AddCalculator>();
+    auto mul_calc = std::make_shared<MultiplyCalculator>();
+
+    add_calc->do_calculation(10, 5);   // 输出 15
+    mul_calc->do_calculation(10, 5);   // 输出 50
+
+    // 用加法替换乘法的回调
+    mul_calc->set_callback(add_calc->get_callback());
+    mul_calc->do_calculation(10, 5);   // 输出 15
+
     return 0;
 }
